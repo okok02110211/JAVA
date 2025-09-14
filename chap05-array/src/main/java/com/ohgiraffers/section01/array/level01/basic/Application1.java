995a26835ad9b829b889e0cab0402e9d5136fb7b
kang seong hun <okok02110211@gmail.com>
2025-09-14 21:35:34 +0900

    완료

---
+++ chap05-array/src/main/java/com/ohgiraffers/section01/array/level01/basic/Application1.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section01.array.level01.basic;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i+1;
+            System.out.println(" "+array[i] + " ");
+        }
+        /* 길이가 10인 정수형 배열을 선언 및 할당한 뒤
+         * 각 인덱스에 차례대로 1부터 10까지 값을 넣고 출력하세요
+         * */
+    }
+
+}
