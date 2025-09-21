6f4c693284e739c55214d98dd4b594c872e5c3db
rkdtjdgus <okok02110211@gmail.com>
2025-09-21 18:04:41 +0900

    ~~Round Proceed 완성

--- Final-project/src/main/java/com/ohgiraffers/Game/Round.java
+++ Final-project/src/main/java/com/ohgiraffers/Game/Round.java
@@ -1,14 +1,38 @@
 package com.ohgiraffers.Game;
 
 import com.ohgiraffers.Players.Player;
+import java.util.Scanner;
 
 public class Round {
     private final Player player;
-
+    private final Scanner sc = new Scanner(System.in);
     public Round(Player player) {
         this.player = player;
     }
     public void Proceed(){
-
+        for(int rerollcount = 2; rerollcount >= 0 ; rerollcount--){
+            System.out.println("======= 라운드 진행합니다 =======");
+            player.Rolling();
+            System.out.println("남은 리롤 횟수 : " + (rerollcount + 1));
+            System.out.println("주사위 다시굴리기 (Y/N) :");
+            String input = sc.nextLine().trim();
+            if(input.equalsIgnoreCase("y")){
+                System.out.println("다시 굴릴 주사위 번호(순서) 를 입력하세요. ex) 1 3 5:");
+                String[] rerollIndex = sc.nextLine().split(" ");
+                int[] parsedIndex = new int[rerollIndex.length];
+                for(int i = 0 ; i < rerollIndex.length; i++){
+                    try{
+                        parsedIndex[i] = Integer.parseInt(rerollIndex[i]) - 1;
+                    }catch(NumberFormatException e){
+                        parsedIndex[i] = -1;
+                    }
+                }
+                player.reRolling(parsedIndex);
+                System.out.println("결과 :" + player.getDiceSet());
+            }else{
+                break;
+            }
+            System.out.println("라운도 종료");
+        }
     }
 }
